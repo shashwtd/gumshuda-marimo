a91e01bbc48e3cf78961028f7ecb45f84643f9f5
cy-bug <shashwat5590@gmail.com>
2022-12-03 20:15:04 +0530

    idk wtf im doin

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from fetch import get_images
-# from faces import faces
+import os
+from PIL import Image
+from dotenv import load_dotenv
+from instabot import Bot
 
+load_dotenv()
+
+username = os.getenv('IG_USERNAME')
+password = os.getenv('IG_PASSWORD')
+
+
+def img(img):
+    if img.endswith('.png'):
+        __ = Image.open(img)
+        _img = __.convert('RGB')
+        _img.save(f'{img[:-3]}.jpg', 'JPEG')
+        os.remove(img)
+        return f'{img[:-3]}.jpg'
+    return img
+
+
+def upload(img, caption):
+    bot.upload_photo(img, caption=caption)
+    # clean_up(img)
+
+    
 if __name__ == "__main__":
-    get_images()
-    # faces()
+    bot = Bot()
+    bot.login(username=username, password=password)
+    upload(img('posts/post.png'), '[test upload]')
